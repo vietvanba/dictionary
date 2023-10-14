233ecb4dd3943b8bd217aa3232aa81230c893fd6
viet van ba <baviet19@gmail.com>
2023-10-14 04:39:40 +0000

    correct endpoint

--- frontend-reactjs/src/API.js
+++ frontend-reactjs/src/API.js
@@ -1,42 +1,40 @@
 import axios from "axios";
-const endpoint = "https://localhost:";
-export async function get(url, port) {
-  return await axios.get(endpoint + port + url, {
+const endpoint = "https://vanbaviet.com:8443";
+export async function get(url) {
+  return await axios.get(endpoint + url, {
     headers: { "Content-type": "application/json" },
   });
 }
-export async function getWithToken(url, port, token) {
-  return await axios.get(endpoint + port + url, {
+export async function getWithToken(url, token) {
+  return await axios.get(endpoint + url, {
     headers: { Authorization: `Bearer ${token}` },
   });
 }
-export async function post(url, port, body) {
-  return await axios.post(endpoint + port + url, body);
+export async function post(url, body) {
+  return await axios.post(endpoint + url, body);
 }
-export async function postWithToken(url, port, body, token) {
-  return await axios.post(endpoint + port + url, body, {
+export async function postWithToken(url, body, token) {
+  return await axios.post(endpoint + url, body, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
     },
   });
 }
-export async function patchWithToken(url, port, body, token) {
-  return await axios.patch(endpoint + port + url, body, {
+export async function patchWithToken(url, body, token) {
+  return await axios.patch(endpoint + url, body, {
     headers: {
       Authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
     },
   });
 }
-export async function patch(url, port, body) {
-  return await axios.patch(endpoint + port + url, body);
+export async function patch(url, body) {
+  return await axios.patch(endpoint + url, body);
 }
-export async function put(url, port, body) {
-  return await axios.put(endpoint + port + url, body);
+export async function put(url, body) {
+  return await axios.put(endpoint + url, body);
 }
-export async function del(url, port, token) {
-  return await axios.delete(endpoint + port + url, {
+export async function del(url, token) {
+  return await axios.delete(endpoint + url, {
     headers: { Authorization: `Bearer ${token}` },
   });
 }
